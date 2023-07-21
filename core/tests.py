349ff944965ba4eae685ecdbe9746b4627fec8e6
Okoli Ifeanyi <Okoli.I@bullseye.local>
2023-07-21 12:22:59 +0100

    line 3 of test commented out

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from rest_framework.test import APIClient, APITestCase
-from models import Customer
+# from models import Customer
 from django.urls import reverse
 
 # Create your tests here.
