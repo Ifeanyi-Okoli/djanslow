e5dc7d370ae9ebed9280b6cc35f2f07f1cb5ca19
Okoli Ifeanyi <Okoli.I@bullseye.local>
2023-07-21 12:16:34 +0100

    Apiview changed to

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.views import index_view
+from core.views import index_view, AddCustomerView
 
 urlpatterns=[
     path('', index_view, name='home'),
+    path('add-customer', AddCustomerView.as_view(), name='add')
 ]
